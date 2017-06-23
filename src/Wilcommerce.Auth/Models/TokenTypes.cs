a5b1b0ea357018b32126322d17280dab0102460c
albi.mori <albi.mori@gmail.com>
2017-06-23 09:18:14 +0200

    service api, token entities

---
+++ src/Wilcommerce.Auth/Models/TokenTypes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Wilcommerce.Auth.Models
+{
+    public static class TokenTypes
+    {
+        public static string PasswordRecovery => "PASSWORD_RECOVERY";
+
+        public static string Registration => "REGISTRATION";
+
+        public static bool IsValidType(string type)
+        {
+            if (string.IsNullOrEmpty(type))
+            {
+                throw new ArgumentNullException("type");
+            }
+
+            bool isValid = typeof(TokenTypes).GetProperties(BindingFlags.Static).Any(p => p.GetValue(null).ToString() == type);
+            return isValid;
+        }
+    }
+}
---
+++ src/Wilcommerce.Auth/Services/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wilcommerce.Auth.Services.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        Task SignIn(string username, string password);
+
+        Task SignOut();
+    }
+}
