89554de89eb7f9b37a437870a7b5cf74a09af2c7
albi.mori <albi.mori@gmail.com>
2017-06-23 14:29:44 +0200

    authenticationservice and defaults

---
+++ src/Wilcommerce.Auth/AuthenticationDefaults.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Wilcommerce.Auth
+{
+    public static class AuthenticationDefaults
+    {
+        public static string AuthenticationScheme => CookieAuthenticationDefaults.AuthenticationScheme;
+
+        public static string CustomerRole => "Customer";
+
+        public static string AdministratorRole => "Administrator";
+
+        public static string CookiePrefix => ".Wilcommerce.";
+    }
+}
---
+++ src/Wilcommerce.Auth/Services/AuthenticationService.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http.Authentication;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Wilcommerce.Core.Common.Domain.Models;
+using Wilcommerce.Core.Common.Domain.ReadModels;
+
+namespace Wilcommerce.Auth.Services
+{
+    public class AuthenticationService : Interfaces.IAuthenticationService
+    {
+        public AuthenticationManager AuthenticationManager { get; }
+
+        public ICommonDatabase CommonDatabase { get; }
+
+        public AuthenticationService(AuthenticationManager authenticationManager, ICommonDatabase commonDatabase)
+        {
+            AuthenticationManager = authenticationManager;
+            CommonDatabase = commonDatabase;
+        }
+
+        public Task SignIn(string email, string password)
+        {
+            try
+            {
+                var user = CommonDatabase.Users
+                    .Where(u => u.IsActive && u.DisabledOn == null)
+                    .FirstOrDefault(u => u.Email == email);
+
+                
+
+                if (user == null)
+                {
+                    throw new InvalidOperationException($"User {email} not found");
+                }
+
+                var principal = CreatePrincipalForUser(user);
+                return AuthenticationManager.SignInAsync(AuthenticationDefaults.AuthenticationScheme, principal);
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+
+        public Task SignOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Protected methods
+        protected virtual ClaimsPrincipal CreatePrincipalForUser(User user)
+        {
+            try
+            {
+                var identity = new ClaimsIdentity();
+                identity.AddClaim(new Claim(ClaimTypes.Name, user.Email));
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+                identity.AddClaim(new Claim(ClaimTypes.Email, user.Email));
+                identity.AddClaim(new Claim(ClaimTypes.Role, GetRoleStringForUser(user)));
+
+                return new ClaimsPrincipal(identity);
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+
+        protected virtual string GetRoleStringForUser(User user)
+        {
+            var role = user.Role;
+            switch (role)
+            {
+                case User.Roles.CUSTOMER:
+                    return AuthenticationDefaults.CustomerRole;
+                case User.Roles.ADMINISTRATOR:
+                    return AuthenticationDefaults.AdministratorRole;
+                default:
+                    return null;
+            }
+        }
+        #endregion
+    }
+}
