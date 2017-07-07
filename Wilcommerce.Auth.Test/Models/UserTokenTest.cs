dae8e50c2a666c7cdf6badbc8af5dfd0a0e9d982
albi.mori <albi.mori@gmail.com>
2017-07-07 11:27:04 +0200

    setup unit test

---
+++ Wilcommerce.Auth.Test/Models/UserTokenTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Wilcommerce.Auth.Models;
+using Xunit;
+
+namespace Wilcommerce.Auth.Test.Models
+{
+    public class UserTokenTest
+    {
+        [Fact]
+        public void PasswordRecovery_Should_Throw_ArgumentNullException_If_User_IsNull()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() => UserToken.PasswordRecovery(null, "", DateTime.Now));
+            Assert.Equal("user", ex.ParamName);
+        }
+    }
+}
