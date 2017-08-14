8c7ad10fbad5d1dba0bee78637098fd699f3a61d
albi.mori <albi.mori@gmail.com>
2017-08-14 15:45:14 +0200

    setup identity factory test

---
+++ Wilcommerce.Auth.Test/Services/IdentityFactoryTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wilcommerce.Auth.Services;
+using Wilcommerce.Auth.Services.Interfaces;
+using Wilcommerce.Core.Common.Domain.Models;
+using Xunit;
+
+namespace Wilcommerce.Auth.Test.Services
+{
+    public class IdentityFactoryTest
+    {
+        private IIdentityFactory _factory;
+
+        public IdentityFactoryTest()
+        {
+            _factory = new IdentityFactory();
+        }
+
+        [Fact]
+        public void CreateIdentity_Identity_Name_Must_Match_User_Email()
+        {
+            var user = User.CreateAsAdministrator("User", "admin@admin.com", "1234");
+            var principal = _factory.CreateIdentity(user);
+
+            Assert.Equal(user.Email, principal.Identity.Name);
+        }
+
+        [Fact]
+        public void AdministratorUser_Must_Have_Administrator_As_Role()
+        {
+            var user = User.CreateAsAdministrator("User", "admin@admin.com", "1234");
+            var principal = _factory.CreateIdentity(user);
+
+            Assert.True(principal.IsInRole(AuthenticationDefaults.AdministratorRole));
+        }
+
+        [Fact]
+        public void CustomerUser_Must_Have_Customer_As_Role()
+        {
+            var user = User.CreateAsCustomer("Customer", "customer@customer.com", "1234");
+            var principal = _factory.CreateIdentity(user);
+
+            Assert.True(principal.IsInRole(AuthenticationDefaults.CustomerRole));
+        }
+    }
+}
--- src/Wilcommerce.Auth/Services/IdentityFactory.cs
+++ src/Wilcommerce.Auth/Services/IdentityFactory.cs
@@ -1,19 +1,25 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Security.Claims;
 using Wilcommerce.Core.Common.Domain.Models;
 
 namespace Wilcommerce.Auth.Services
 {
     public class IdentityFactory : Interfaces.IIdentityFactory
     {
-        public ClaimsPrincipal CreateIdentity(User user)
+        public virtual ClaimsPrincipal CreateIdentity(User user)
         {
             try
             {
+                if (user == null)
+                {
+                    throw new ArgumentNullException("user");
+                }
+
                 var identity = new ClaimsIdentity();
                 identity.AddClaim(new Claim(ClaimTypes.Name, user.Email));
                 identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
                 identity.AddClaim(new Claim(ClaimTypes.Email, user.Email));
-                identity.AddClaim(new Claim(ClaimTypes.Role, GetRoleStringForUser(user)));
+                identity.AddClaim(new Claim(ClaimTypes.Role, GetUserRoleAsString(user)));
                 identity.AddClaim(new Claim(ClaimTypes.GivenName, user.Name));
 
                 return new ClaimsPrincipal(identity);
@@ -29,7 +35,7 @@ namespace Wilcommerce.Auth.Services
         /// </summary>
         /// <param name="user">The user instance</param>
         /// <returns>The user's role as a string</returns>
-        protected virtual string GetRoleStringForUser(User user)
+        protected virtual string GetUserRoleAsString(User user)
         {
             var role = user.Role;
             switch (role)
